1f8459837c96879a3246da19e4b510ef2ee33879
DAndris <pajszerm5@gmail.com>
2024-04-03 18:57:01 +0200

    initial commit

---
+++ src/main/java/com/example/imageproject/ImageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imageproject/ImageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
