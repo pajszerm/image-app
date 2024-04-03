d7c015186ecc6251499d1b4cdcb18d643cf82e1d
DAndris <pajszerm5@gmail.com>
2024-04-03 18:57:32 +0200

    initial commit

--- src/test/java/com/example/imageproject/ImageProjectApplicationTests.java
+++ src/test/java/com/example/imageproject/ImageProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ImageProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
